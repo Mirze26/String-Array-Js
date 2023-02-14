f55bcbdb7fbd8ab94d5862fbe05c7fa625b2860e
unknown <mirzaab@code.edu.az>
2023-02-14 06:40:10 +0400

    String Rewerse

---
+++ Practica/assets/js/script.js
@@ -0,0 +1,140 @@
+"use strict";
+
+// function getStudentCountByEmail(str){
+
+// let students = getStudents();
+// let count = 0;
+
+// for (const stu of students) {
+//     if (stu.email.includes(str)) {
+//         count++;
+        
+//     }
+// }
+
+// return count;
+
+// }
+
+// console.log(getStudentCountByEmail("z"))
+
+
+
+// const getBirthdayYear = age =>{
+//     let date = new Date();
+//     let year = date.getFullYear();
+//     return year - age;
+// }
+// console.log(getBirthdayYear(27))
+
+
+// function getStudents(){
+
+
+//     let stu1={
+//         name:"Mirze",
+//         email:"mizo@gmail.com"
+//     }
+    
+    
+    
+//     let stu2={
+//         name:"Hemze",
+//         email:"masan@gmail.com"
+//     }
+    
+    
+    
+    
+//     let stu3={
+//         name:"Hemid",
+//         email:"hemid@gmail.com"
+//     }
+    
+    
+    
+    
+//     let stu4={
+//         name:"Heyder",
+//         email:"prince@gmail.com"
+//     }
+    
+//     let students = [stu1,stu2,stu3,stu4];
+//     return students;
+
+
+
+// }
+
+//------------------------------------------
+
+
+// let address = "BulBule";
+// for (let i = 0; i < address.length; i++) {
+//     console.log(address[i])
+    
+// }
+
+// console.log(address[1])
+// console.log(address.charAt(2))
+// console.log(address.charCodeAt(3))
+//console.log(address.toUpperCase)
+//console.log(address.split(""))
+// console.log(address.trim())
+//console.log(address.trimStart())
+//console.log(address.slice(1))
+//console.log(address.substring(1))
+
+// let text = "Deyildim men sene mail , sen etdun eqlimi zail";
+// if(text.length>10){
+//     console.log(text.substring(10) + " ...")
+// }
+
+//--------------------------------------------------------
+
+
+// let text = "Salam Mubariz bey";
+// console.log(text.replace("Mubariz", "Anar"))
+
+//-----------------------------------------------------
+
+// const strToUpper = str =>{
+//     return str.charAt(0).toUpperCase() + str.slice(1)
+// }
+//console.log(strToUpper("salam"))
+
+// let text ="salam";
+// let result = text[0].toLocaleUpperCase();
+// let res = text.replace(text.charAt(0),result)
+// console.log(res)
+
+//--------------------------------------------------------
+
+
+//let students =["Cavid","Shahin","Pishiy","Ceyhun","Gulbala"]
+
+// console.log(students[students.length-1])
+// let result = students.join()
+//console.log(result)
+
+// students.push("Elekber");
+// console.log(students)
+
+// function filter(arr){
+//     for (const item of arr) {
+//         if(item.includes("y")){
+//             console.log(item)
+//         }
+//     }
+// }
+// filter(students)
+
+//------------------------------------------------------
+
+
+// let arr =[10,20,30,40,5,6,7];
+// console.log(arr.findIndex(m=>m>20))
+
+
+// let age ="55";
+// console.log(typeof age.toString())
